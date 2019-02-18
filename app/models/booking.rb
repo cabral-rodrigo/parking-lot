13fdf3cbf8dc95b47561f5c126ef7cb23c11010b
Nicolas Caif <r.nicolascaif@gmail.com>
2019-02-18 18:36:38 -0300

    modified schema and models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :parking
   belongs_to :user
+  has_many :reviews
   validates :availability, presence: true
   validates :status, presence: true
   validates :date, presence: true
--- app/models/parking.rb
+++ app/models/parking.rb
@@ -1,14 +1,17 @@
 class Parking < ApplicationRecord
   SIZE_CAR = ["Small Car", "Medium Car", "Big Car", "Truck"]
   belongs_to :user
+  has_many :bookings
+  has_many :reviews, through: :bookings
+  has_many :users, through: :bookings
   validates :address, presence: true
   validates :picture, presence: true
   validates :confirmation, presence: true
-  validates :covered, presence: true, default: false
-  validates :security, presence: true, default: false
-  validates :camera, presence: true, default: false
+  validates :covered, presence: true
+  validates :security, presence: true
+  validates :camera, presence: true
   validates :size, inclusion: { in: SIZE_CAR }
-  validates :gated, presence: true, default: false
+  validates :gated, presence: true
   validates :price, presence: true
   validates :price, numericality: { only_integer: true }
 end
