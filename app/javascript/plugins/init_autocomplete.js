d9608e5e2fbabf3c18d3f6895584827e3018ef99
rodrigo cabral <cabral-rodrigo@hotmail.com>
2019-02-20 18:59:08 -0300

    search restricted for form

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,9 +1,14 @@
 import places from 'places.js';
 
+const boundries = {
+  countries: ['ar'] // only search in the United States, the rest of the settings are unchanged: we're still searching for cities in German.
+};
+
 const initAutocomplete = () => {
   const addressInput = document.getElementById('parking_address');
   if (addressInput) {
-    places({ container: addressInput });
+    places({ container: addressInput }).configure(boundries);
+
   }
 };
 
